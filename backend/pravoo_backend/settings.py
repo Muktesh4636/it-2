09ddbb12fd89fa72ff4e5a0a4c0f8413d33b5e94
Muktesh4636 <mukteshreddy4636@gmail.com>
2026-01-18 01:53:36 +0530

    Fix HTML page loading: move static files to proper directory and update references

--- backend/pravoo_backend/settings.py
+++ backend/pravoo_backend/settings.py
@@ -50,7 +50,7 @@ ROOT_URLCONF = 'pravoo_backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -110,8 +110,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- backend/pravoo_backend/urls.py
+++ backend/pravoo_backend/urls.py
@@ -1,11 +1,23 @@
 """
 URL configuration for pravoo_backend project.
 """
+from django.shortcuts import render
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+def home(request):
+    return render(request, "index.html")
 
 urlpatterns = [
+    path('', home),
     path('admin/', admin.site.urls),
     path('api/', include('contact.urls')),
 ]
+
+# Serve static files in development
+if settings.DEBUG:
+    from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+    urlpatterns += staticfiles_urlpatterns()
 
